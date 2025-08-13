afb9a917ccbcb0d2f24fbdef8eabb50066c47670
= <=>
2025-08-13 22:42:26 +0300

    implement

--- car/serializers.py
+++ car/serializers.py
@@ -11,13 +11,13 @@ class CarSerializer(serializers.Serializer):
     horse_powers = serializers.IntegerField()
     is_broken = serializers.BooleanField()
     # може бути null і може бути відсутнім у вхідних даних
-    problem_description = serializers.CharField(allow_null=True, required=False, allow_blank=True)
+    problem_description = serializers.CharField(allow_null=True,
+                                                required=False, allow_blank=True)
 
     def create(self, validated_data):
-        """Створює Car та проганяє модельні валідатори (у т.ч. min/max для horse_powers)."""
         car = Car(**validated_data)
         try:
-            car.full_clean()  # підхоплює MinValueValidator/MaxValueValidator з моделі
+            car.full_clean()
         except DjangoValidationError as e:
             # перетворюємо на DRF ValidationError
             raise serializers.ValidationError(e.message_dict or e.messages)
